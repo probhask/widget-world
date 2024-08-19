ba98c59fa5f7e85b837e0cb80c0603eec6d75362
Bhaskar Sharma <bhaskarneeraj23@gmail.com>
2024-08-20 00:59:55 +0530

    update- icon color to purple

--- src/components/DisplayWidget/InfoMessage.tsx
+++ src/components/DisplayWidget/InfoMessage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiOutlinePlusCircle } from "react-icons/ai";
+import { AiFillPlusCircle, AiOutlinePlusCircle } from "react-icons/ai";
 
 const InfoMessage = React.memo(() => {
   return (
@@ -7,9 +7,9 @@ const InfoMessage = React.memo(() => {
       <h4 className="uppercase text-2xl font-semibold text-center">
         add some widget from store
       </h4>
-      <h6 className="flex items-center justify-center flex-wrap gap-y-0.5 gap-x-2 text-lg text-center   ">
-        <span className="flex gap-0.5 flex-wrap items-center justify-center">
-          Click <AiOutlinePlusCircle className="text-green-700 text-2xl" /> icon
+      <h6 className="flex items-center justify-center flex-wrap gap-y-0.5 gap-x-2 text-lg text-center mt-3">
+        <span className="flex gap-2 flex-wrap items-center justify-center">
+          Click <AiFillPlusCircle className="text-[#671488] text-2xl" /> icon
         </span>
         at the top-right corner to create new
       </h6>
