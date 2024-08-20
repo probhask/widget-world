20632b5893dbb7e4d4ef3821daad2e418421e417
Bhaskar Sharma <bhaskarneeraj23@gmail.com>
2024-08-20 17:58:56 +0530

    add -serach icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { FaStore } from "react-icons/fa";
+import { FaSearch, FaStore } from "react-icons/fa";
 import { HiPlus } from "react-icons/hi";
 import { IoMdAddCircle } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 const Header = React.memo(() => {
   return (
-    <header className="flex items-center justify-between px-2 min-w-[220px]">
-      <h1 className="sm:w-full lg:text-center px-2 font-bold mid-tab:text-3xl text-4xl md:text-6xl uppercase text-[#674188]">
+    <header className="flex flex-col gap-3 sm:gap-10 items-center justify-between px-2 min-w-[220px]">
+      <h1 className="sm:w-full text-center px-2 font-bold text-3xl  sm:text-6xl uppercase text-[#674188]">
         Widget World
       </h1>
-      <div className="flex items-center gap-4 sm:gap-2 text-[#674188]">
+      <div className="flex items-center gap-3 sm:gap-2 text-[#674188]">
+        <Link
+          to="/store/search"
+          className="cursor-pointer active:scale-90 transition-all sm:border border-[#671488] sm:py-1 sm:px-2 sm:rounded-md flex items-center gap-1  sm:hover:text-[#F7EFE5] sm:hover:bg-[#671488] rounded-full bg-[#671488] sm:bg-inherit text-[#F7EFE5] h-6 w-6 sm:w-max sm:h-max justify-center sm:text-inherit"
+          title="header-create-new"
+        >
+          <FaSearch />
+          <span className="hidden sm:block">search</span>
+        </Link>
         <Link
           to="/store/create-new?category=new"
           className="cursor-pointer active:scale-90 transition-all sm:border border-[#671488] sm:py-1 sm:px-2 rounded-md flex items-center gap-1 w-max sm:hover:text-[#F7EFE5] sm:hover:bg-[#671488]"
@@ -18,7 +26,6 @@ const Header = React.memo(() => {
         >
           <HiPlus className="hidden sm:block text-xl  " />
           <IoMdAddCircle className="sm:hidden text-3xl" />
-
           <span className="hidden sm:block">Create New</span>
         </Link>
         <Link
