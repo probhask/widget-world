9529417a8902aa6649a72bf257c641a52d3aeee1
Bhaskar Sharma <bhaskarneeraj23@gmail.com>
2024-08-24 08:07:19 +0530

    add - errorboundary and suspense

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,21 +1,31 @@
+import ErrorBoundary from "@components/ErrorBoundary/ErrorBoundary";
 import ErrorElement from "@components/ErrorElement/ErrorElement";
 import WidgetApp from "@container/WidgetApp";
 import Search from "@features/Store/Search/Search";
 import Available from "@features/Store/StoreTabNavigate/Available/Available";
 import CreateNew from "@features/Store/StoreTabNavigate/CreateNew/CreateNew";
 import StoreTabNavigate from "@features/Store/StoreTabNavigate/StoreTabNavigate";
-import WidgetStore from "@features/Store/WidgetStore";
+import { lazy, Suspense } from "react";
+const WidgetStore = lazy(() => import("../features/Store/WidgetStore"));
 import { createBrowserRouter } from "react-router-dom";
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <WidgetApp />,
+    element: (
+      <ErrorBoundary>
+        <WidgetApp />
+      </ErrorBoundary>
+    ),
     errorElement: <ErrorElement />,
     children: [
       {
         path: "store",
-        element: <WidgetStore />,
+        element: (
+          <Suspense>
+            <WidgetStore />
+          </Suspense>
+        ),
         children: [
           {
             path: "",
