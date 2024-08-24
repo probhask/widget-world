6486055f0e261b75b702a6abf9f11f900b932183
Bhaskar Sharma <bhaskarneeraj23@gmail.com>
2024-08-24 08:02:28 +0530

    add - error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import ErrorBoundary from "@components/ErrorBoundary/ErrorBoundary.tsx";
+import { BiLoaderCircle } from "react-icons/bi";
+const App = React.lazy(() => import("./App"));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <Suspense
+      fallback={
+        <div className="w-screen h-screen flex justify-center items-center">
+          <BiLoaderCircle className="size-10 md:size-20 animate-spin text-[#671488] duration-75 ease-in" />
+        </div>
+      }
+    >
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </Suspense>
   </React.StrictMode>
 );
